f9f270a71f09ffadfd86c4cd9893e2ee55748b6f
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-02-17 01:01:02 +0900

    feat: bi from cdn

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 <body>
     {{-- flash --}}
