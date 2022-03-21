bd17d8553d48dbb0d95e1e31e04f4b772630acbf
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-03-22 01:03:24 +0900

    chore: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
+            ClientSeeder::class,
+            ItemSeeder::class,
         ]);
     }
 }
