e23b3a2a481bddb9742bf1cb86a4adf8b3658dd0
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-03-23 11:54:55 +0900

    fix: inquiry value old

--- resources/views/inquiries/form.blade.php
+++ resources/views/inquiries/form.blade.php
@@ -14,11 +14,11 @@
         <div class="col-12">
             <div class="mb-3">
                 <label for="formTitleInput" class="form-label">タイトル</label>
-                <input type="text" class="form-control" id="formTitleInput" name="title" placeholder="タイトル" value="{{ $inquiry->title }}" required>
+                <input type="text" class="form-control" id="formTitleInput" name="title" placeholder="タイトル" value="{{ old('title', $inquiry->title) }}" required>
             </div>
             <div class="mb-3">
                 <label for="formBodyTextArea" class="form-label">本文</label>
-                <textarea class="form-control" id="formBodyTextArea" name="body" rows="3" placeholder="Description here." required>{{ $inquiry->body }}</textarea>
+                <textarea class="form-control" id="formBodyTextArea" name="body" rows="3" placeholder="Description here." required>{{ old('body', $inquiry->body) }}</textarea>
             </div>
         </div>
     </div>
