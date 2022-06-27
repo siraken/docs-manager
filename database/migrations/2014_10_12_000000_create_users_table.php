b295865a2f7b0ce8e386fbd09e31429e1852bbd6
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-06-27 11:46:49 +0900

    feat: Login with NFC (security improve required)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nfc_serial_number')->nullable()->unique();
+            $table->string('nfc_pin')->nullable()->unique();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -8,6 +8,7 @@ require("./bootstrap");
 
 require("./status");
 require("./jquery/jquery");
+require("./nfc-auth");
 
 /**
  * Next, we will create a fresh React component instance and attach it to
